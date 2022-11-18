b4e5d60538cf326cbcacdcf47d40910b42affa6c
Liam Egan <liam@wethecollective.com>
2022-11-18 09:18:33 -0800

    Fixing number deserialization to allow for negative numbers

--- project/src/index.js
+++ project/src/index.js
@@ -7,6 +7,24 @@ const sp = new URLSearchParams(window.location.search);
 
 console.log(sp);
 
+const params = [];
+params.push({
+  name: "It's a number",
+  type: "number",
+  default: "0.8",
+  options: {
+    min: "0",
+    max: "2",
+    step: "0.05",
+  },
+});
+params.push({
+  name: "A color",
+  type: "color",
+  default: "ff000000",
+});
+$fx.params(params);
+
 // this is how to define parameters
 $fx.params([
   {
@@ -16,7 +34,7 @@ $fx.params([
     options: {
       min: "0",
       max: "2",
-      step: "0.05",
+      step: "0.00005",
     },
   },
   {
@@ -50,6 +68,7 @@ $fx.features({
   "Feature from params, its a number": $fx.param("It's a number"),
 })
 
+console.log($fx.param("It's a number"));
 
 // log the parameters, for debugging purposes, artists won't have to do that
 console.log("Current param values:")
