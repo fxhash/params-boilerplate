793a741f69f2185dee8e3442c968551625e429fc
maerzhase <markus@lili16.de>
2022-12-26 11:56:39 +0100

    adjust params to match spec

--- project/src/index.js
+++ project/src/index.js
@@ -28,35 +28,52 @@ $fx.params(params);
 // this is how to define parameters
 $fx.params([
   {
+    id: "number_id",
     name: "It's a number",
     type: "number",
     default: "0.8",
     options: {
-      min: "0",
-      max: "2",
-      step: "0.00005",
+      max: 2,
+      min: 1,
+      step: 0.05,
     },
   },
   {
-    name: "A color",
-    type: "color",
-    default: "ff000000",
+    id: "select_id",
+    name: "A selection",
+    type: "select",
+    default: "orange",
+    options: {
+      options: ["apple", "orange", "pear"],
+    }
   },
   {
+    id: "color_id",
+    name: "A color",
+    type: "color",
+    default: "#ff000000",
+  },
+  {
+    id: "boolean_id",
     name: "A boolean",
     type: "boolean",
     default: "true",
   },
   {
+    id: "string_id",
     name: "A string",
     type: "string",
-    default: "hello",
+    default: "helloxx",
+    options: {
+      minLength: 1,
+      maxLength: 3
+    }
   },
   {
-    name: "A selection",
-    type: "select",
-    default: "orange",
-    options: ["apple", "orange", "pear"],
+    id: "string_id2",
+    name: "A string",
+    type: "string",
+    default: "hello3",
   },
 ]);
 
@@ -65,10 +82,10 @@ $fx.features({
   "A random feature": Math.floor(fxrand() * 10),
   "A random boolean": fxrand() > 0.5,
   "A random string": ["A", "B", "C", "D"].at(Math.floor(fxrand()*4)),
-  "Feature from params, its a number": $fx.param("It's a number"),
+  "Feature from params, its a number": $fx.param("number_id"),
 })
 
-console.log($fx.param("It's a number"));
+console.log($fx.param("number_id"));
 
 // log the parameters, for debugging purposes, artists won't have to do that
 console.log("Current param values:")
